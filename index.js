670f21e7c73311cf44ee51165da7f3f51d78aa41
jaspreetsingh <jaspreetbrar1636@gmail.com>
2020-06-18 18:22:41 +0530

    completed till the rentals

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
+const mongoose = require('mongoose');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 const express = require('express');
 const app = express();
 
-const mongoose = require('mongoose');
-//setting up the mongoose
-mongoose.set('useNewUrlParser', true);
-mongoose.set('useFindAndModify', false);
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-//connect to mongodb
-mongoose.connect('mongodb://localhost:27017/vidly')
-    .then(_ => console.log('connected'))
-    .catch(err => console.log('could not connect to db'));
+mongoose.connect('mongodb://localhost/vidly')
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
 
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
